c19116b92fffac6580a1ce8714e4a3667a8ff421
icancode23 <ernipunarora@gmail.com>
2017-08-21 19:26:25 +0530

    Added Task Interface to django views

--- RouteMatcher/MicroService/tasks.py
+++ RouteMatcher/MicroService/tasks.py
@@ -1,9 +1,13 @@
 from celery.decorators import task
+from celery import shared_task
 from celery.utils.log import get_task_logger
+import time
 
+logger = get_task_logger(__name__)
 
-@task
+@shared_task
 def rebuild_search_index():
-     time.sleep(500) # mimicking a long running process
+     time.sleep(20) # mimicking a long running process
      print('rebuilt search index')
+
      return 42
