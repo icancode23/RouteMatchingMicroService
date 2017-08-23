5f289a4e6889863467920226b4b0b4843ad783fa
icancode23 <ernipunarora@gmail.com>
2017-08-23 11:25:45 +0530

    Started Endpoint for route matching

--- RouteMatcher/RouteMatcher/urls.py
+++ RouteMatcher/RouteMatcher/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url (r'^check$','MicroService.views.check',name="Check"),
     url (r'^sendNotification$','MicroService.views.sendNotifications',name="sendNotifications"),
-    url (r'^matchTrip$','MicroService.matchTrip',name="MatchTrip")
+    url (r'^matchTrip$','MicroService.views.matchTrip',name="matchTrip")
 )
